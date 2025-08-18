ca3ee8929106f8bdd2b7ced69eb87851c6b0c6cc
Pankaj Kumar Jha <pankaj.jha@bounteous.com>
2025-08-18 13:33:51 +0530

    UPDATED FILE 2

--- 6.js
+++ 6.js
@@ -1,26 +1,20 @@
-function get_balance(){
-    let balance = 0 ;
-    function check_balance(){
-        return balance ; 
-    } 
-    function deposit(amount){
-        balance +=amount 
-
-    }
-    function credit(amount){
-        if(amount > 0 && amount <= balance){
-            balance -= amount;
-        }
-    }
-    return {
-        check_balance,
-        deposit,
-        credit
-    };
-
+function get_chennai_weather(callback){
+    const lat = 13.0827;
+	const lon = 80.2707;
+	const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {callback(null,data.current_weather)})
+        .catch(err => {callback(err,null)})
 }
-const my_account = get_balance();
-my_account.deposit(5000);
-console.log(my_account.check_balance()); 
-my_account.credit(2000);
-console.log(my_account.check_balance()); 
+function show_weather(err,data){
+    if(err){
+        console.error("Error fetching weather data:", err);
+    } else {
+        console.log("Current weather in Chennai:", data.temperature);
+        console.log("Weather condition:", data.weathercode);
+        console.log("Wind speed:", data.windspeed);
+        console.log("Humidity:", data.humidity);    
+    }
+}
+get_chennai_weather(show_weather);
